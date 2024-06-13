ec6d954bd067468680ffb810d224d26765dd76c1
RRaihan1234 <raihanspare123@gmail.com>
2024-06-13 13:09:42 +0600

    Developed the image gallery app

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import { useState } from 'react';
+import './App.css';
+import allData from '../src/data.json'
+
+function App() {
+  const [images, setImages] = useState(allData.data);
+  const [deletableArray] = useState([]);
+  const [, setRender] = useState(false);
+
+  const handleCheck = (id) => {               // For checking, whether the checkbox inside image is selected or not
+        for(let i = 0; i<images.length; i++){
+          if(id===images[i].id){
+            if(images[i].selected===false){
+              images[i].selected=true
+              deletableArray.push(1)
+            }else{
+              images[i].selected=false
+              deletableArray.length = deletableArray.length - 1;
+            }
+          }
+        }
+        setRender((prevState)=>!prevState)
+  }
+
+  const handleDelete = () => {  // For deleting images
+        let imagesAfterDeletion = images.filter((image)=>image.selected===false);
+        setImages(imagesAfterDeletion);
+        deletableArray.length=0;
+  }
+
+  const handleDragStart = (e, id) => {   // For drag operation
+        e.dataTransfer.setData("text/plain", id.toString());
+  }
+
+  const handleDragEnd = (e) => {   // For drag operation
+        e.dataTransfer.clearData();
+  }
+
+  const handleDrop = (e, dropId) => {   // For drop operation
+    setRender((prevState)=>!prevState)
+    e.preventDefault();
+    const dragId = e.dataTransfer.getData("text/plain");
+    let dragIdIndex = images.findIndex((image)=>image.id === Number(dragId))
+    let dropIdIndex = images.findIndex((image)=>image.id === dropId)
+    if(dragIdIndex === dropIdIndex || Number(dragId) === allData.data.length - 1 || dropId === allData.data.length - 1){
+      setImages(images)
+    }else if(dragIdIndex > dropIdIndex){
+      let tmp = images[dragIdIndex]
+      let i;
+      for(i = dragIdIndex; i>=dropIdIndex; i--){
+         images[i] = images[i-1]
+      }
+      images[dropIdIndex]=tmp;
+    }else{
+      let tmp = images[dragIdIndex]
+      let i;
+      for(i = dragIdIndex; i<=dropIdIndex; i++){
+         images[i] = images[i+1]
+      }
+      images[dropIdIndex]=tmp;
+    }
+  };
+
+  const handleDragOver = (e) => {  // For drag operation
+    e.preventDefault();
+  };
+
+  return (
+    <div className="App">
+       <div className="parentDiv">
+          <div className='topSection'>
+              <div className="topSectionHeading">
+                  {deletableArray.length === 0 && <h3>Gallery</h3>}
+                  {deletableArray.length > 0 && <div className="topDelete">
+                      <div>
+                        <input type='checkbox' checked readOnly/>
+                        {deletableArray.length === 1 && <h3 className="leftText">1 file Selected</h3>}
+                        {deletableArray.length > 1 && <h3 className="leftText">{deletableArray.length} files Selected</h3>}
+                      </div>
+                      <div>
+                        {deletableArray.length === 1 && <h3 className="rightText" onClick={handleDelete}>Delete file</h3>}
+                        {deletableArray.length > 1 && <h3 className="rightText" onClick={handleDelete}>Delete files</h3>}
+                      </div>
+                  </div>}
+              </div>
+              <div className="topSectionLine"></div>
+          </div>
+          <div className="imageGallery">
+        {images.map((image)=>{
+           return(
+                 <>
+                    {image.id!==allData.data.length - 1 && 
+                    <div 
+                    draggable 
+                    onDragStart = {(e)=>handleDragStart(e, image.id)}
+                    onDragEnd = {handleDragEnd}
+                    onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, image.id)}
+                    key={image.id} 
+                    className={image.selected?"singleImageDivShadow":"singleImageDiv"}>
+                    <img src={require(`../src/images/${image.image}`)} className={image.selected?'singleImageShadow':'singleImage'} alt='singleImg'></img>
+                    <input type='checkbox' className={image.selected?'showCheck':'check'} onChange={()=>handleCheck(image.id)}/>
+                   </div>}
+                    {image.id===allData.data.length - 1 && 
+                    <div key={image.id} 
+                    className="addImageDiv"
+                    onDragStart = {(e)=>handleDragStart(e, image.id)}
+                    onDragEnd = {handleDragEnd}
+                    onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, image.id)}>
+                    <img src={require(`../src/images/${image.image}`)} className='addImage' alt='addImg'></img>
+                   </div>}
+                 </>
+           )
+        })}
+    </div>
+       </div>
+    </div>
+  );
+}
+
+export default App;
